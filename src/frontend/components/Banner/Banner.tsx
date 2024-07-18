4370d3af45a7844867d0c419ed21ae5d3f61b3db
ShreyaChk <Shreya.chakraborty2109@gmail.com>
2024-07-18 11:35:10 +0530

    Update Banner.tsx
    RUM code

--- src/frontend/components/Banner/Banner.tsx
+++ src/frontend/components/Banner/Banner.tsx
@@ -2,9 +2,25 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import Link from 'next/link';
+import { useEffect } from 'react';
 import * as S from './Banner.styled';
 
 const Banner = () => {
+    console.log("InsideBanner.tsx")
+  useEffect(() => {
+      console.log("Banner.tsx:::useEffect")
+      const script = document.createElement('script');
+      script.src = '../public/scripts/acn-genwiz-rum-monitor.js';
+      script.async = true;
+      document.body.appendChild(script);
+      console.log("RUMdsfsfdfd")
+       return () => {
+         document.body.removeChild(script);
+       };
+  }, []);
+ console.log("Banner.tsx:::beforereturn")
+
+
   return (
     <S.Banner>
       <S.ImageContainer>
